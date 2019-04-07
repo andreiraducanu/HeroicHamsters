447eb9c0898260cd832cabe574161cea67a309f5
Raducanu Andrei <raducanu.andrei98@gmail.com>
2019-04-07 10:42:32 +0300

    suport for eslint

--- training/src/server.ts
+++ training/src/server.ts
@@ -1,18 +1,18 @@
-import express from "express";
-import morgan from "morgan";
-import bodyParser from "body-parser";
+import express from 'express';
+import morgan from 'morgan';
+import bodyParser from 'body-parser';
 
 const app = express();
-const port = 4000; // default port to listen
+const port = 3000; // default port to listen
 
-//  set up our application to report and generate logs files 
-app.use(morgan("common"));
+//  set up our application to report and generate logs files
+app.use(morgan('common'));
 
 // support parsing of application/json type post data
 app.use(bodyParser.json());
 
 // define a route handler for the default home page
-app.get("/", (req, res) => {
+app.get('/', (req, res) => {
     res.send('Hello World!');
 });
 

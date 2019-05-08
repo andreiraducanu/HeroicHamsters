9d3db37bf9e4c3f0d56cd63e0e3d733195d3a398
Raducanu Andrei <raducanu.andrei98@gmail.com>
2019-05-08 18:13:48 +0300

    added .gitignore

--- backend_v2/src/index.ts
+++
@@ -1,8 +0,0 @@
-import App from './App';
-
-import UserController from './controllers/UserController';
-import AdminController from './controllers/AdminController';
-
-const app = new App([new UserController(), new AdminController()]);
-
-app.listen();

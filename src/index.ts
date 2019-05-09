85cc2328cae2cfccd08f2893e1feb6bf11470f11
Raducanu Andrei <raducanu.andrei98@gmail.com>
2019-05-09 14:28:48 +0300

    added help controller

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,8 @@ import App from './App';
 
 import UserController from './controllers/UserController';
 import AdminController from './controllers/AdminController';
+import HelpController from './controllers/HelpController';
 
-const app = new App([new UserController(), new AdminController()]);
+const app = new App([new UserController(), new AdminController(), new HelpController()]);
 
 app.listen();
